aea35198dabcf6a1a94de43e64ebb20c9c711f2d
srijashuva <srijashuva@gmail.com>
2026-02-19 16:59:01 +0530

    modified  cs file for render

--- Program.cs
+++ Program.cs
@@ -45,5 +45,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
